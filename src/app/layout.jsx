87fbe436bbfe3a821fdc12888ab268e63fa540e5
mugunth140 <mugunth140@gmail.com>
2025-02-11 16:30:41 +0530

    Added vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import {ReactLenis} from "@/utils/lenis";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.scss";
 
 <Head>
